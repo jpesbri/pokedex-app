636eba88f50fc61b35f2d0e2ea2065ce29a47550
jpesbri <juan.espana@fxreplay.com>
2025-11-09 16:27:45 -0400

    chore: update dependencies and add NestJS support
    - Added NestJS packages: @nestjs/common, @nestjs/core, @nestjs/platform-express, @nestjs/schematics, @nestjs/testing
    - Updated axios and reflect-metadata versions
    - Updated @types/node to version 20.19.9
    - Added esbuild and webpack-cli to dependencies
    - Updated paths in tsconfig.base.json for module resolution

---
+++ libs/domain/src/index.ts
@@ -0,0 +1,5 @@
+export * from './lib/domain';
+
+export * from './lib/entities/pokemon.entity';
+export * from './lib/value-objects/pokemon-type.enum';
+export * from './lib/repositories/pokemon.repository.interface';
